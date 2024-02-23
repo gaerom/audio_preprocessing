e47e0781eb0a2a197d4ca9313ed3e24ca114cbdd
Saerom Kim <92725975+gaerom@users.noreply.github.com>
2024-02-23 15:46:29 +0900

    raw video에서 audio 추출

---
+++ preprocessing/extract_audio.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+from glob import glob
+
+video_dir = '/home/broiron/Desktop/TPoS/dataset/raw_videos'
+audio_dir = '/home/broiron/Desktop/TPoS/dataset/raw_audios'
+
+os.makedirs(audio_dir, exist_ok=True)
+
+video_files = glob(os.path.join(video_dir, '*.mp4'))
+
+for video_path in video_files:
+    base_name = os.path.basename(video_path)
+    file_name = os.path.splitext(base_name)[0]
+
+    audio_output_path = os.path.join(audio_dir, file_name + '.wav')
+
+    # .wav 형식으로 encoding
+    command = f'ffmpeg -i "{video_path}" -acodec pcm_s16le -ar 44100 -ac 2 "{audio_output_path}"'
+    subprocess.run(command, shell=True)
