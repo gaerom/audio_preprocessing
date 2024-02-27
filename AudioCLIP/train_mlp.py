fb475efc3adfff5d2c93e88863cbaff1ccd2231d
Saerom Kim <92725975+gaerom@users.noreply.github.com>
2024-02-27 20:42:59 +0900

    Update train_mlp.py

--- AudioCLIP/train_mlp.py
+++ AudioCLIP/train_mlp.py
@@ -18,36 +18,33 @@ from IPython.display import Audio, display
 sys.path.append(os.path.abspath(f'{os.getcwd()}/..'))
 
 from model import AudioCLIP # audio encoder
+from model.model_final import FrozenCLIPTextEmbedder # text encoder
 from utils.transforms import ToTensor1D
 
 
 torch.set_grad_enabled(False)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 MODEL_FILENAME = 'AudioCLIP-Full-Training.pt'
 # derived from ESResNeXt
 SAMPLE_RATE = 44100 # 초당 44100개의 sample을 사용하여 audio signal을 디지털화
-# derived from CLIP
-IMAGE_SIZE = 224
-IMAGE_MEAN = 0.48145466, 0.4578275, 0.40821073
-IMAGE_STD = 0.26862954, 0.26130258, 0.27577711
 
-# LABELS = ['cat', 'thunderstorm', 'coughing', 'alarm clock', 'car horn'] # text label(ESC-50)
 # UnAV-100 annotation과 같은 것(VGGSound/data/Common.txt에 있는 label과 동일)
 LABELS = ['beat boxing', 'cat purring', 'cattle, bovinae cowbell', 'fire truck siren', 'playing violin, fiddle'] # vggsound sample audio에 해당하는 label
 
-# pre-trained audio encoder
-aclp = AudioCLIP(pretrained=f'../assets/{MODEL_FILENAME}')
-aclp.eval() # 어차피 audio encoder를 학습시키는게 아니니까
+# audio encoder
+audio_encoder = AudioCLIP(pretrained=f'../assets/{MODEL_FILENAME}')
+audio_encoder.eval() # 어차피 audio encoder를 학습시키는게 아니니까
 
-audio_transforms = ToTensor1D() # audio feature를 1D tensor로 변환하기 위한 것, 왜..? -> 일단 pytorch에서 다루기 쉽게 만들기 위해서
+# text encoder
+text_encoder = FrozenCLIPTextEmbedder(version='RN50', device=device)
 
-
-# demo/audio/*.wav (5개)
 #paths_to_audio = glob.glob('./vggsound/raw_audios/*.wav') # audio input은 .wav / vggsound data로 경로 수정
 paths_to_audio_segments = glob.glob('./vggsound/segments/*.wav')
 
 audio = list()
 audio_data = {}
+audio_transforms = ToTensor1D() # audio feature를 1D tensor로 변환하기 위한 것, 왜..? -> 일단 pytorch에서 다루기 쉽게 만들기 위해서
 for path in paths_to_audio_segments:
     video_id = '_'.join(path.split('/')[-1].split('_')[:-2])
     
@@ -65,20 +62,48 @@ all_audio_features = []
 for i, (video_id, segments) in enumerate(audio_data.items()):
     for segment in segments:
         audio_sample = segment.unsqueeze(0)
-        print(f'통과 전 shape: {audio_sample.shape}') # (1, 1, 88200)
+        print(f'통과 전 audio feature shape: {audio_sample.shape}') # (1, 1, 88200)
         # audio encoder 통과
         with torch.no_grad():
-            ((audio_features, _, _), _), _ = aclp(audio=audio_sample)
-        print(f'encoder 통과한 audio feature shape {i+1}: {audio_features.shape}')
+            ((audio_features, _, _), _), _ = audio_encoder(audio=audio_sample)
+        print(f'encoder 통과한 audio embedding shape {i+1}: {audio_features.shape}')
         all_audio_features.append(audio_features.squeeze(0)) 
 
 all_audio_features = torch.stack(all_audio_features)
 
-text = [[label] for label in LABELS] 
-((_, _, text_features), _), _ = aclp(text=text) # text encoder는 CLIP 기반의 text encoder로 교체해야 함
+# text = [[label] for label in LABELS] 
+for i in LABELS:
+    text_features = text_encoder.encode(i)
+    print(f'Text embedding shape(GT): {text_features.shape}')  # (1, 77, 1024)
 
 
-print(text_features.shape)
+""" Audio embedding과 Text embedding MSE 적용"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -106,8 +131,9 @@ print(text_features.shape)
 
 """
 1. audio encoder를 통과 시켰음 -> audio embedding 차원 확인 완료
-
-
 2. text encoder(CLIP text encoder)로 text embedding 뽑아야 함
+
+
+
 3. 1, 2 사이의 MSE 적용해야 함
 """
