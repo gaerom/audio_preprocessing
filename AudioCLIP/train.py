13b7f76f6f96bf4709958f46bf33d63bd3860e46
Saerom Kim <92725975+gaerom@users.noreply.github.com>
2024-03-05 17:15:33 +0900

    add dataloader
    train, validation dataset에 대해 dataloader 추가

--- AudioCLIP/train.py
+++ AudioCLIP/train.py
@@ -4,7 +4,7 @@
 2-1. text encoder(CLIP text encoder)로 text embedding 뽑음. 이때 차원은 [1, 77, 1024]
 3. 2, 2-1 사이에 MSE를 적용 -> audio embedding[1, 1024]가 MLP를 거쳤을 때 text embedding[1, 77, 1024]가 되도록
 """
-""" To-do: validation까지 적용 """
+
 
 import os
 import sys
@@ -15,16 +15,18 @@ import librosa.display
 
 import numpy as np
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
 
 sys.path.append(os.path.abspath(f'{os.getcwd()}/..'))
 
 from model import AudioCLIP # audio encoder
 from model.model_final import FrozenCLIPTextEmbedder # text encoder
-
+from dataset import AudioDataset # dataset
 
 from utils.transforms import ToTensor1D
 
@@ -34,7 +36,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 max_length = 77
 input_dim = 1024
 output_dim = 1024
-epochs = 20
+epochs = 50
+
 
 class Mapping_Model(nn.Module):
     def __init__(self, input_dim=1024, output_dim=1024, max_length=77):
@@ -49,6 +52,7 @@ class Mapping_Model(nn.Module):
         x = self.linear2(x)
         return x.view(-1, max_length, output_dim) 
 
+
 model = Mapping_Model(input_dim, output_dim, max_length).to(device)
 loss_function = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -59,48 +63,106 @@ SAMPLE_RATE = 44100
 
 
 # audio encoder
-audio_encoder = AudioCLIP(pretrained=f'../assets/{MODEL_FILENAME}')
-audio_encoder.eval() # 어차피 audio encoder를 학습시키는게 아니니까
+audio_encoder = AudioCLIP(pretrained=f'../assets/{MODEL_FILENAME}').eval().to(device) # 학습시키는게 아니니까
+# text encoder, 나중에 ViT 기반으로 교체
+text_encoder = FrozenCLIPTextEmbedder(version='RN50', device=device).eval().to(device)
 
-# text encoder
-text_encoder = FrozenCLIPTextEmbedder(version='RN50', device=device)
 
-#paths_to_audio = glob.glob('./vggsound/raw_audios/*.wav')
 # 아래 경로는 각각의 10초 raw audio를 2초 segment로 잘라서 저장해놓은 경로
-# 40만개는 load하다가 process 죽음 -> segments_2_val: 38,430개
-audio_segments_path = '/home/broiron/Desktop/AudioCLIP/data/test_2/*.wav' # 여기에 2초짜리 audio segment(.wav)
+audio_segments_path = '/home/broiron/Desktop/AudioCLIP/data/test_2/*.wav'
+audio_paths = glob.glob(audio_segments_path)
+
 
 audio = list()
-audio_data = {}
 audio_transforms = ToTensor1D()
-for path in tqdm(glob.glob(audio_segments_path), desc='Audio loading: '):
-    video_id = '_'.join(path.split('/')[-1].split('_')[:-2])
-    track, _ = librosa.load(path, sr=SAMPLE_RATE, dtype=np.float32)
-    transformed_track = audio_transforms(track.reshape(1, -1))
+audio_data = [audio_transforms(librosa.load(path, sr=SAMPLE_RATE, dtype=np.float32)[0].reshape(1, -1)) for path in tqdm(audio_paths, desc='Audio loading: ')]
+
+# for path in tqdm(glob.glob(audio_segments_path), desc='Audio loading: '):
+#     video_id = '_'.join(path.split('/')[-1].split('_')[:-2])
+#     track, _ = librosa.load(path, sr=SAMPLE_RATE, dtype=np.float32)
+#     transformed_track = audio_transforms(track.reshape(1, -1))
     
-    if video_id not in audio_data:
-        audio_data[video_id] = [transformed_track]
-    else:
-        audio_data[video_id].append(transformed_track)
+#     if video_id not in audio_data:
+#         audio_data[video_id] = [transformed_track]
+#     else:
+#         audio_data[video_id].append(transformed_track)
 
 
 # Load labels from file
-labels_path = '/home/broiron/Desktop/AudioCLIP/data/label/labels.txt'
+# dataset 파일명에서 추출한 label
+labels_path = '/home/broiron/Desktop/AudioCLIP/data/label/test_labels.txt'
 with open(labels_path, 'r') as file:
     labels = [line.strip() for line in file]
 
-all_audio_features = [] 
-for i, (video_id, segments) in tqdm(enumerate(audio_data.items()), desc='Audio processing: '):
-    for segment in segments:
-        audio_sample = segment.unsqueeze(0)
-        # print(f'통과 전 audio feature shape: {audio_sample.shape}') # (1, 1, 88200)
-        # audio encoder 통과
+
+### train / validation 분리
+audio_data_train, audio_data_val, labels_train, labels_val = train_test_split(audio_data, labels, test_size=0.2, random_state=42)
+print(f'train: {len(audio_data_train)}')
+
+# Audio encoding function
+def encode_audio_data(audio_data_list):
+    encoded_features = []
+    for audio_sample in tqdm(audio_data_list, desc='Audio encoding: '):
         with torch.no_grad():
             ((audio_features, _, _), _), _ = audio_encoder(audio=audio_sample)
-        # print(f'encoder 통과한 audio embedding shape {i+1}: {audio_features.shape}')
-        all_audio_features.append(audio_features.squeeze(0)) 
+            # audio_features = audio_encoder(audio_sample.unsqueeze(0).to(device))[0]
+            print(f'encoder 통과한 audio embedding shape: {audio_features.shape}') # [1, 1024]
+            encoded_features.append(audio_features)
+    return torch.stack(encoded_features)
 
-all_audio_features = torch.stack(all_audio_features)
+# train, validation data 각각 encoding 처리
+audio_features_train = encode_audio_data(audio_data_train)
+audio_features_val = encode_audio_data(audio_data_val)
+
+
+# # audio encoder 통과
+# all_audio_features = [] 
+# for i, (video_id, segments) in tqdm(enumerate(audio_data.items()), desc='Audio processing: '):
+#     for segment in segments:
+#         audio_sample = segment.unsqueeze(0)
+#         # print(f'통과 전 audio feature shape: {audio_sample.shape}') # (1, 1, 88200)
+        
+#         with torch.no_grad():
+#             ((audio_features, _, _), _), _ = audio_encoder(audio=audio_sample)
+#         print(f'encoder 통과한 audio embedding shape {i+1}: {audio_features.shape}') # [1, 1024]
+#         all_audio_features.append(audio_features) 
+        
+# all_audio_features = torch.stack(all_audio_features) # audio embedding
+
+
+batch_size = 64
+shuffle = True
+num_workers = 0 # default: 0
+
+# Data Loader
+train_dataset = AudioDataset(audio_features_train, labels_train)
+val_dataset = AudioDataset(audio_features_val, labels_val)
+
+train_loader = DataLoader(train_dataset, 
+                          batch_size=batch_size, 
+                          shuffle=shuffle, num_workers=num_workers)
+
+val_loader = DataLoader(val_dataset, 
+                        batch_size=batch_size, 
+                        shuffle=False, 
+                        num_workers=num_workers)
+
+
+# validation
+def evaluate(model, data_loader, loss_function):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad(): 
+        for audio_features, labels_batch in data_loader:
+            audio_features = audio_features.to(device).float()
+            text_embeddings = torch.stack([text_encoder.encode([label]).to(device).float() for label in labels_batch])
+
+            predictions = model(audio_features)
+            loss = loss_function(predictions, text_embeddings.squeeze(1))
+            total_loss += loss.item()
+
+    avg_loss = total_loss / len(data_loader)
+    return avg_loss
 
 
 
@@ -110,59 +172,39 @@ cnt = 0
 best_loss = float('inf')  # 최소 손실 값을 저장하기 위한 변수 초기화
 model_save_path = '/home/broiron/Desktop/AudioCLIP/assets/train_best_model.pth'  
 
+# training
 model.train()
-for epoch in tqdm(range(epochs), desc='Training: '):
+for epoch in range(epochs):
     total_loss = 0
-    for i, audio_feature in enumerate(all_audio_features):
-        audio_feature = audio_feature.unsqueeze(0).to(device).float()
-        
-        # Match each audio feature with its corresponding text label
-        label = labels[i % len(labels)]  
-        text_embedding = text_encoder.encode([label]).to(device).float()
-        # print(f'Text embedding shape(GT): {text_embedding.shape}')
-        
+    for audio_features, labels_batch in train_loader:
+        audio_features = audio_features.to(device).float() 
+
+        # text embedding 추출
+        text_embeddings = torch.stack([text_encoder.encode([label]).to(device).float() for label in labels_batch])
+        # print(f'text embedding shape: {text_embeddings.shape}')
+
         optimizer.zero_grad()
-        audio_output = model(audio_feature)
-        # MSE 계산
-        loss = loss_function(audio_output, text_embedding)
+        predictions = model(audio_features) # MLP 통과한 embedding
+        loss = loss_function(predictions, text_embeddings.squeeze(1)) # 앞서 stack으로 쌓아서 차원이 하나 더 생김(?) -> squeeze로 제거
         loss.backward()
         optimizer.step()
-        
+
         total_loss += loss.item()
 
-    avg_loss = total_loss / len(all_audio_features)
-    print(f'Epoch {epoch+1}, Loss: {total_loss / avg_loss}')
+    avg_train_loss = total_loss / len(train_loader)
+    avg_val_loss = evaluate(model, val_loader, loss_function)
 
-    if avg_loss < best_loss:
-        best_loss = avg_loss
+    print(f'Epoch {epoch+1}: Train Loss = {avg_train_loss}, Val Loss = {avg_val_loss}')
+
+    if avg_val_loss < best_loss:
+        best_loss = avg_val_loss
         torch.save(model.state_dict(), model_save_path)
         print(f"Best model saved at epoch: {epoch+1}, loss: {best_loss}")
-        cnt = 0  
+        cnt = 0
     else:
         cnt += 1
 
-    # Early Stopping 체크
     if cnt >= patience:
         print(f"Early stopping {epoch+1}")
-        break 
-
-
-
-# ### Normalize Embedding
-# all_audio_features = all_audio_features / torch.linalg.norm(all_audio_features, dim=-1, keepdim=True)
-# text_features = text_features / torch.linalg.norm(text_features, dim=-1, keepdim=True)
-
-# ### Similarity 계산
-# scale_audio_text = torch.clamp(aclp.logit_scale_at.exp(), min=1.0, max=100.0)
-# logits_audio_text = scale_audio_text * all_audio_features @ text_features.T
-
-# # Audio Classification
-# confidence = logits_audio_text.softmax(dim=1)
-
-# # 출력 부분 수정
-# print('\t\tFilename, Audio\t\t\tTextual Label (Confidence)', end='\n\n')
-# for audio_idx, path in enumerate(paths_to_audio_segments):
-#     conf_values, ids = confidence[audio_idx].topk(3)
-#     query = f'{os.path.basename(path):>30s} ->\t\t'
-#     results = ', '.join([f'{LABELS[i]:>15s} ({v:06.2%})' for v, i in zip(conf_values, ids)])
-#     print(query + results)
+        break
+    
