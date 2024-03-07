6f2e567c5afef5a540e43c476c097cfc13d50340
Saerom Kim <92725975+gaerom@users.noreply.github.com>
2024-03-07 11:46:26 +0900

    Update extract_label.py

--- AudioCLIP/preprocessing/extract_label.py
+++ AudioCLIP/preprocessing/extract_label.py
@@ -1,22 +1,20 @@
-""" 파일명에서 text label 추출 """
+""" 가진 dataset에 대해 순서대로 label 추출 """
 import os
 from tqdm import tqdm
 
-video_dir = '/home/broiron/Desktop/AudioCLIP/data/vggsound/train'
+seg_dir = '/home/broiron/Desktop/AudioCLIP/data/segments'
 label_dir = '/home/broiron/Desktop/AudioCLIP/data/label'
 
+# Ensure the label directory exists
 if not os.path.exists(label_dir):
     os.makedirs(label_dir)
 
 labels_file_path = os.path.join(label_dir, 'labels.txt')
 
-# 중복 제거
-labels_set = set()
+# file 순서대로 label 추출 (dataset 개수와 동일)
+sorted_filenames = sorted(os.listdir(seg_dir))
 
 with open(labels_file_path, 'w') as labels_file:
-    for filename in tqdm(os.listdir(video_dir), desc='Extracting labels'):
+    for filename in tqdm(sorted_filenames, desc='Extracting labels'):
         label = filename.split('_')[0]
- 
-        if label not in labels_set:
-            labels_file.write(label + '\n')
-            labels_set.add(label)
+        labels_file.write(label + '\n')
